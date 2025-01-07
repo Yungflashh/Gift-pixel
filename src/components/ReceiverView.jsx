8546e18b40a79aafe7b6f47dc64a886ae9523ef7
yungflashh <streaktv8@gmail.com>
2025-01-07 09:37:35 +0100

    trying to add analytics

--- src/components/ReceiverView.jsx
+++ src/components/ReceiverView.jsx
@@ -78,8 +78,9 @@ const ReceiverView = () => {
                 Cookies.set("shareToken", shareToken)
                 try {
                     // Send the share token to the server to track access
+                    shareToken2 = "43dd70cd-9600-46aa-8a92-e5c65449b403"
                     const response = await axios.get('https://auth-zxvu.onrender.com/api/auth/track', {
-                        params: { shareToken: "43dd70cd-9600-46aa-8a92-e5c65449b403" }
+                        params: { shareToken2}
                     });
                     if (response.data.success) {
                         console.log('Link access tracked successfully');
